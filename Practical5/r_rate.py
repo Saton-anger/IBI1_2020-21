0f6378fc91920ec396111872ddf3af45364ec4a8
Saton-anger <79832629+Saton-anger@users.noreply.github.com>
2021-03-10 18:10:53 +0800

    Update r_rate.py

--- Practical5/r_rate.py
+++ Practical5/r_rate.py
@@ -2,7 +2,9 @@ n=84
 r=1.2
 add=n
 for i in range(1,6):
+#calculate the new students who are infected
     add=add*r
+#calculate the total students
     n=n+add
 print("the r rate is:",r,"  number of infected individuals after 5 rounds of infectionn:",n)
 
