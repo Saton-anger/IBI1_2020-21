dec3168e8e4197e2a27b4ae462690b7e52e2b0cf
Saton-anger <79832629+Saton-anger@users.noreply.github.com>
2021-03-17 10:32:54 +0800

    version 1.2

--- Practical6/list manipulation 2.py
+++ Practical6/list manipulation 2.py
@@ -2,11 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 gene_lengths = [9410, 394141, 4442, 105338, 19149, 76779, 126550, 36296, 842, 15981]
 exon_counts = [51, 1142, 42, 216, 25, 650, 32533, 57, 1, 523]
+# change list to array to calculate easily
 a = np.array(gene_lengths)
 b = np.array(exon_counts)
 eal = a / b
 eal.sort()
-eal = list(eal)
+eal = list(eal) # change array to list to avoid the disappearance of ","
 print()
 print(eal)
 plt.boxplot(eal,
