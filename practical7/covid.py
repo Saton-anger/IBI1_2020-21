519ba73f5ce74e854f3376bb487926b1e3edada5
Saton-anger <79832629+Saton-anger@users.noreply.github.com>
2021-05-18 10:12:28 +0800

    delete some  pseudocode

--- practical7/covid.py
+++ practical7/covid.py
@@ -8,9 +8,6 @@ covid_data = pd.read_csv("full_data.csv")
 
 print(covid_data.iloc[0:10:2, 0:5])
 
-# my_columns = [True, True, False, True, False, False]
-# print(covid_data.iloc[0:3, my_columns])
-
 my = []
 for i in range(7996):
     if covid_data.loc[i, 'location'] == "Afghanistan":
@@ -18,7 +15,6 @@ for i in range(7996):
     else:
         my.append(False)
 print(covid_data.loc[my, 'total_cases'])
-
 
 a1 = []
 for i in range(7996):
@@ -45,11 +41,6 @@ plt.boxplot(world_new_cases,
 )
 plt.title('box plot for world new cases')
 plt.show()
-
-#world_dates = covid_data.loc[a1, 'date']
-#plt.plot(world_dates, world_new_cases, 'ro')
-#plt.xticks(world_dates.iloc[0:len(world_dates):6], rotation=-45)
-#plt.show()
 
 
 
