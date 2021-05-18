c6e6c401af9e1e67b22c823472ffa3a5dc4a91c1
Saton-anger <79832629+Saton-anger@users.noreply.github.com>
2021-05-18 09:38:21 +0800

    add some pseudocode to make the meaning of the code more clear

--- Practical5/fibonacci.py
+++ Practical5/fibonacci.py
@@ -1,8 +1,9 @@
+# initialize the series, give the original two words
 a=1
 b=1
 print(a)
 print(b)
-#loop
+# loop
 for i in range(0,11):
 	c=b
 	b=a+b
--- Practical5/r_rate.py
+++ Practical5/r_rate.py
@@ -1,10 +1,12 @@
 n=84
 r=1.2
 add=n
+# The	rate	of	reproduction	of	a	virus,	e.g.	COVID-19,	can	be	quantified	using	the	‘r’	number	which
+# is	the	average	number	of	individuals	infected	by	each	individual	with	the	virus.	An	r	rate	of	1.2
+# means	that	each	infected	individual	will	on	average	infect	1.2	individuals.
 for i in range(1,6):
-#calculate the new students who are infected
+# calculate the new students who are infected
     add=add*r
-#calculate the total students
+# calculate the total students
     n=n+add
 print("the r rate is:",r,"  number of infected individuals after 5 rounds of infectionn:",n)
-
